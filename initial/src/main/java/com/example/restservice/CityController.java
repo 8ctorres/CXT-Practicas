4f77631ccd4fd0602d11f51d149e2b2feaae8ae0
Carlos Torres <carlos.torres@udc.es>
2021-04-18 13:35:56 +0200

    Gettting 404 error. Working on it...

--- initial/src/main/java/com/example/restservice/CityController.java
+++ initial/src/main/java/com/example/restservice/CityController.java
@@ -1,13 +1,35 @@
 package main.java.com.example.restservice;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/*
 @RestController
 public class CityController {
     private List<City> cities = new ArrayList<City>(10);
-    cities.add(new City(1, "Coruña", "Coruña"));
-    cities.add(new City(2, "Vigo", "Pontevedra"));
+    public CityController(){
+        cities.add(new City(1, "Coruña", "Coruña"));
+        cities.add(new City(2, "Vigo", "Pontevedra"));
+    };
 
     @GetMapping("/cities")
     public List<City> cities(){
         return cities;
+    }
+}
+
+*/
+
+@RestController
+public class CityController{
+    @GetMapping("/cities")
+    public City city(
+             @RequestParam(value = "name", defaultValue = "lacoru") String name
+    ){
+        return new City(54, name, "Coruña");
     }
 }
