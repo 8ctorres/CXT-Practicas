8d2123ab08fc2d7292363a06e3a94389c6199966
Ismael Verde Costas <ismael.verdec@udc.es>
2021-04-23 11:57:20 +0200

    Añadidas comunidades autonomas a una lista y creada clase comunnitie

--- complete/src/main/java/com/example/springboot/CityController.java
+++ complete/src/main/java/com/example/springboot/CityController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 @RestController
 public class CityController {
     private List<City> cities = new ArrayList<City>(20);
+    private List<Comunnitie> comunnities = new ArrayList<Comunnitie>(20);
 
     public CityController(){
         cities.add(new City(1, "A Coruña", "A Coruña"));
@@ -31,6 +32,26 @@ public class CityController {
         cities.add(new City(18, "Marín", "Pontevedra"));
         cities.add(new City(19, "Cangas", "Pontevedra"));
         cities.add(new City(20, "O Porriño", "Pontevedra"));
+
+        comunnities.add(new Comunnitie(1, "Andalucía", "Sevilla"));
+        comunnities.add(new Comunnitie(2, "Aragón", "Zaragoza"));
+        comunnities.add(new Comunnitie(3, "Principado de Asturias", "Oviedo"));
+        comunnities.add(new Comunnitie(4, "Islas Baleares", "Palma de Mallorca"));
+        comunnities.add(new Comunnitie(5, "Canarias", "Santa Cruz de Tenerife"));
+        comunnities.add(new Comunnitie(6, "Cantabria", "Santander"));
+        comunnities.add(new Comunnitie(7, "Castilla la Mancha", "Toledo"));
+        comunnities.add(new Comunnitie(8, "Castilla y León", "Valladolid"));
+        comunnities.add(new Comunnitie(9, "Cataluña", "Barcelona"));
+        comunnities.add(new Comunnitie(10, "Ceuta", "Ceuta"));
+        comunnities.add(new Comunnitie(11, "Extremadura", "Mérida"));
+        comunnities.add(new Comunnitie(12, "Galicia", "Santiago de Compostela"));
+        comunnities.add(new Comunnitie(13, "La Rioja", "Logroño"));
+        comunnities.add(new Comunnitie(14, "Comunidad de Madrid", "Madrid"));
+        comunnities.add(new Comunnitie(15, "Melilla", "Melilla"));
+        comunnities.add(new Comunnitie(16, "Región de Murcia", "Murcia"));
+        comunnities.add(new Comunnitie(17, "Navarra", "Pamplona"));
+        comunnities.add(new Comunnitie(18, "Comunidad Valenciana", "Valencia"));
+        comunnities.add(new Comunnitie(19, "País Vasco", "Vitoria"));
     }
 
 	  @RequestMapping("/cities")
